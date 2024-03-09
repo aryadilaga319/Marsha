b0d47396cb715b18ac54a8453aaa96794f6aac6b
aryadilaga319 <aprianadilaga@gmail.com>
2024-03-09 10:59:48 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', () => {
+    Swal.fire({
+        title: 'Do you want to play music in the background?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes',
+        cancelButtonText: 'No',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            document.querySelector('.song').play();
+            animationTimeline();
+        } else {
+            animationTimeline();
+        }
+    });
+});
+
+
+let menu = document.querySelector('#menu-icon');
+let meki = document.querySelector('.meki');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    meki.classList.toggle('open');
+};
+
+
+
